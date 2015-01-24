fee1221baedb848093298e2d6a351ed7b6f99a21
robert berry <berrydigital@gmail.com>
2015-01-24 10:15:36 -0800

    reading rules files, but only storing a single last regular expression for comparison

--- dict_exclude.c
+++ dict_exclude.c
@@ -13,6 +13,8 @@
 #include "regex/regex.h"
 #include "utils/elog.h"
 #include "catalog/pg_collation.h"
+#include "tsearch/ts_public.h"
+#include "tsearch/ts_locale.h"
 
 #define DICT_EXCLUDE_BACKREF_CNT    10
 
@@ -20,10 +22,10 @@ PG_MODULE_MAGIC;
 
 typedef struct
 {
-	char*			rule_file;
-	regex_t   rules;
+  regex_t   *rules;
 } DictExclude;
 
+int process_rule_file(DictExclude *d);
 
 PG_FUNCTION_INFO_V1(dict_exclude_init);
 PG_FUNCTION_INFO_V1(dict_exclude_lexize);
@@ -31,53 +33,33 @@ PG_FUNCTION_INFO_V1(dict_exclude_lexize);
 Datum
 dict_exclude_init(PG_FUNCTION_ARGS)
 {
-  int compres;
-  pg_wchar   *wstr;
-  int     wlen;
-	List	   *dictoptions = (List *) PG_GETARG_POINTER(0);
-	DictExclude    *d;
-	ListCell   *l;
-  char* default_regexp = "abc";
+  List     *dictoptions = (List *) PG_GETARG_POINTER(0);
+  DictExclude    *d;
+  ListCell   *l;
 
-	d = (DictExclude *) palloc0(sizeof(DictExclude));
-	d->rule_file = "dict_exclude.rules";
+  d = (DictExclude *) palloc0(sizeof(DictExclude));
 
-	foreach(l, dictoptions)
-	{
-		DefElem    *defel = (DefElem *) lfirst(l);
-
-		if (pg_strcasecmp(defel->defname, "RULEFILE") == 0)
-		{
-			d->rule_file = defGetString(defel);
-		}
-		else
-		{
-			ereport(ERROR,
-					(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-					 errmsg("unrecognized intdict parameter: \"%s\"",
-							defel->defname)));
-		}
-	}
-
-  /*Manually set a single rule for debug, replace with rule list TODO*/
-  
-  wstr = (pg_wchar *) palloc((strlen(default_regexp) + 1) * sizeof(pg_wchar));
-  wlen = pg_mb2wchar_with_len(default_regexp, wstr, strlen(default_regexp));
-  
-  compres = pg_regcomp(&d->rules, wstr, wlen, REG_ADVANCED, DEFAULT_COLLATION_OID);
-  if (compres != REG_OKAY)
+  foreach(l, dictoptions)
   {
-    char    errstr[100];
+    DefElem    *defel = (DefElem *) lfirst(l);
 
-      pg_regerror(compres, &d->rules, errstr, sizeof(errstr));
+    // TODO think of some parameters? and remove unused rulefile option
+    if (pg_strcasecmp(defel->defname, "RULEFILE") == 0)
+    {
+      elog(LOG, "skipping unsused parameter");
+    }
+    else
+    {
       ereport(ERROR,
-          (errcode(ERRCODE_INVALID_REGULAR_EXPRESSION),
-           errmsg("invalid regular expression: %s", errstr)));
+          (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+           errmsg("unrecognized intdict parameter: \"%s\"",
+              defel->defname)));
+    }
   }
-  
-  pfree(wstr);
+
+  process_rule_file(d);
       
-	PG_RETURN_POINTER(d);
+  PG_RETURN_POINTER(d);
 }
 
 Datum
@@ -87,18 +69,18 @@ dict_exclude_lexize(PG_FUNCTION_ARGS)
   regmatch_t  pmatch[DICT_EXCLUDE_BACKREF_CNT];
   pg_wchar   *data;
   size_t    data_len;
-	DictExclude    *d = (DictExclude *) PG_GETARG_POINTER(0);
-	char	   *in = (char *) PG_GETARG_POINTER(1);
-	char	   *txt = pnstrdup(in, PG_GETARG_INT32(2));
-	TSLexeme   *res = palloc0(sizeof(TSLexeme) * 2);
+  DictExclude    *d = (DictExclude *) PG_GETARG_POINTER(0);
+  char     *in = (char *) PG_GETARG_POINTER(1);
+  char     *txt = pnstrdup(in, PG_GETARG_INT32(2));
+  TSLexeme   *res = palloc0(sizeof(TSLexeme) * 2);
 
-	res[1].lexeme = NULL;
-	
-	//re = RE_compile_and_cache(d->rules, REG_ADVANCED, PG_GET_COLLATION());
+  res[1].lexeme = NULL;
+  
+  //re = RE_compile_and_cache(d->rules, REG_ADVANCED, PG_GET_COLLATION());
   data = (pg_wchar *) palloc((strlen(txt) + 1) * sizeof(pg_wchar));
   data_len = pg_mb2wchar_with_len(txt, data, strlen(txt));
   
-	regexec_result = pg_regexec(&d->rules,
+  regexec_result = pg_regexec(&d->rules,
                               data,
                               data_len,
                               0, /* search start position */
@@ -108,15 +90,75 @@ dict_exclude_lexize(PG_FUNCTION_ARGS)
                               0);
 
   if (regexec_result == REG_NOMATCH)
-	{
-		res[0].lexeme = txt;
+  {
+    res[0].lexeme = txt;
   }
-	else
-	{
-		/* reject by returning void array */
-		pfree(txt);
-		res[0].lexeme = NULL;
-	}
+  else
+  {
+    /* reject by returning void array */
+    pfree(txt);
+    res[0].lexeme = NULL;
+  }
 
-	PG_RETURN_POINTER(res);
+  PG_RETURN_POINTER(res);
+}
+
+
+
+int process_rule_file(DictExclude *d) 
+{
+  int compres;
+  pg_wchar   *wstr;
+  int     wlen;
+  char     *line;
+  tsearch_readline_state trst;
+  char     *filename = get_tsearch_config_filename("exclude", "rules");
+  
+  if (!tsearch_readline_begin(&trst, filename))
+      ereport(ERROR,
+          (errcode(ERRCODE_CONFIG_FILE_ERROR),
+           errmsg("could not open stop-word file \"%s\": %m",
+              filename)));
+  
+  while ((line = tsearch_readline(&trst)) != NULL)
+  {
+
+    char     *pbuf = line;
+
+    /* Trim trailing space */
+    while (*pbuf && !t_isspace(pbuf))
+      pbuf += pg_mblen(pbuf);
+    *pbuf = '\0';
+    
+    elog(LOG, "reading a line: %s", line);
+    
+    //skip empty lines
+    if (*line == '\0')
+    {
+      pfree(line);
+      continue;
+    }
+    
+    /* Generate a regular expression */
+    wstr = (pg_wchar *) palloc((strlen(line) + 1) * sizeof(pg_wchar));
+    wlen = pg_mb2wchar_with_len(line, wstr, strlen(line));
+    
+    compres = pg_regcomp(&d->rules, wstr, wlen, REG_ADVANCED, DEFAULT_COLLATION_OID);
+    if (compres != REG_OKAY)
+    {
+      char    errstr[100];
+
+      pg_regerror(compres, &d->rules, errstr, sizeof(errstr));
+      ereport(ERROR,
+             (errcode(ERRCODE_INVALID_REGULAR_EXPRESSION),
+              errmsg("invalid regular expression: %s", errstr)));
+    }
+    
+    pfree(wstr);
+    
+    
+  }
+  
+  return 0;
+
 }
